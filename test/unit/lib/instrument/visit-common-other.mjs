336c6fcd848a6bc34985f82b3d6d19b773b530ba
Laurent Christophe <lachrist@vub.ac.be>
2021-03-24 16:52:06 +0100

    now, only the root location has the file and namespace

---
+++ test/unit/lib/instrument/visit-common-other.mjs
@@ -0,0 +1,9 @@
+import { strict as Assert } from 'assert';
+import {
+  getResultNode,
+  getResultEntities,
+} from '../../../../lib/instrument/result.mjs';
+import { visitExpression } from '../../../../lib/instrument/visit.mjs';
+import '../../../../lib/instrument/visit-common-other.mjs';
+
+
