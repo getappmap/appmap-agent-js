eacc6f2c88266fb75f5dc49f3349a6eee0c4ea4a
Laurent Christophe <lachrist@vub.ac.be>
2021-03-26 13:45:05 +0100

    visit tested

--- test/unit/lib/instrument/visit.mjs
+++ test/unit/lib/instrument/visit.mjs
@@ -1,134 +1,168 @@
 import { strict as Assert } from 'assert';
 import {
-  getResultNode,
-  getResultEntities,
-  combineResult,
-} from '../../../../lib/instrument/result.mjs';
-import {
   visit,
-  assignVisitorObject,
+  setVisitor,
+  getEmptyArray,
+  getEmptyResult,
 } from '../../../../lib/instrument/visit.mjs';
 
-/////////////
-// Dummies //
-/////////////
+Assert.deepEqual(getEmptyArray(), []);
 
-{
-  const node = { type: 'InvalidNodeType' };
-  const location2 = { __proto__: null };
-  [
-    ['MethodDefinition', 'MethodDefinition'],
-    ['ClassBody', 'ClassBody'],
-    ['Method', 'FunctionExpression'],
-    ['Expression', 'Identifier'],
-    ['SpreadableExpression', 'Identifier'],
-    ['TemplateElement', 'TemplateElement'],
-    ['TemplateLiteral', 'TemplateLiteral'],
-    ['NonComputedMemberProperty', 'Identifier'],
-    ['NonComputedKey', 'Identifier'],
-    ['Property', 'Property'],
-    ['PropertyPattern', 'Property'],
-    ['RestablePropertyPattern', 'Identifier'],
-    ['Pattern', 'Identifier'],
-    ['RestablePattern', 'Identifier'],
-    ['Program', 'Program'],
-    ['SwitchCase', 'SwitchCase'],
-    ['BlockStatement', 'BlockStatement'],
-    ['CatchClause', 'CatchClause'],
-    ['VariableDeclarator', 'VariableDeclarator'],
-    ['VariableDeclaration', 'VariableDeclaration'],
-    ['ScopingIdentifier', 'Identifier'],
-    ['NonScopingIdentifier', 'Identifier'],
-    ['ImportSpecifier', 'NamedImportSpecifier'],
-    ['ExportSpecifier', 'ExportSpecifier'],
-    ['Literal', 'Literal'],
-    ['Statement', 'ExpressionStatement'],
-  ].forEach(([kind, type]) => {
-    const location1 = {
-      __proto__: null,
-      extend: (...args) => {
-        Assert.deepEqual(args, [kind, node]);
-        return location2;
-      },
-    };
-    const result = visit(kind, node, location1);
-    Assert.equal(getResultNode(result).type, type, kind);
-    Assert.deepEqual(getResultEntities(result), [], kind);
-  });
-}
+Assert.deepEqual(getEmptyResult(), {
+  node: null,
+  entities: [],
+});
 
-////////////////////////////////
-// shouldBeInstrumented: true //
-////////////////////////////////
+const namespace = 'namespace';
 
-{
-  const node1 = {
-    type: 'Program',
-    body: [],
-    sourceType: 'script',
-  };
-  const node2 = {
-    type: 'Program',
-    body: [],
-    sourceType: 'script',
-  };
-  const location2 = {
+const file = 'file';
+
+const input = {
+  type: 'Identifier',
+  name: 'x',
+};
+
+const output = {
+  type: 'Identifier',
+  name: 'y',
+};
+
+[
+  [true, 'entity0'],
+  [false, null],
+].forEach(([instrumentable, entity], index) => {
+  const extended = {
     __proto__: null,
     shouldBeInstrumented(...args) {
-      Assert.equal(this, location2);
-      Assert.deepEqual(args, []);
-      return true;
+      Assert.equal(this, extended);
+      Assert.deepEqual(args, [file]);
+      return instrumentable;
+    },
+    makeEntity(...args) {
+      Assert.equal(this, extended);
+      Assert.deepEqual(args, [['entity1', 'entity2', 'entite3'], file]);
+      return entity;
     },
   };
-  const location1 = {
+  const location = {
     __proto__: null,
     extend(...args) {
-      Assert.equal(this, location1);
-      Assert.deepEqual(args, ['Program', node1]);
-      return location2;
+      Assert.equal(this, location);
+      Assert.deepEqual(args, [input]);
+      return extended;
     },
   };
-  assignVisitorObject('Program', {
-    Program: (...args) => {
-      Assert.deepEqual(args, [node1, location2]);
-      return combineResult((node, location) => node2, args[0], args[1]);
+  setVisitor(
+    'Identifier',
+    (...args) => {
+      Assert.equal(instrumentable, true);
+      Assert.deepEqual(args, [input, { location: extended, namespace, file }]);
+      return [
+        {
+          node: 'child1',
+          entities: ['entity1', 'entity2'],
+        },
+        [
+          {
+            node: 'child2',
+            entities: ['entite3'],
+          },
+        ],
+      ];
     },
-  });
-  const result = visit('Program', node1, location1);
-  Assert.equal(getResultNode(result), node2);
-  Assert.deepEqual(getResultEntities(result), []);
-}
+    (...args) => {
+      Assert.equal(instrumentable, true);
+      Assert.deepEqual(args, [
+        input,
+        { location: extended, namespace, file },
+        'child1',
+        ['child2'],
+      ]);
+      return output;
+    },
+  );
 
-/////////////////////////////////
-// shouldBeInstrumented: false //
-/////////////////////////////////
+  Assert.deepEqual(
+    visit(input, { location, namespace, file }),
+    instrumentable
+      ? {
+          node: output,
+          entities:
+            entity === null ? ['entity1', 'entity2', 'entite3'] : [entity],
+        }
+      : {
+          node: input,
+          entities: [],
+        },
+  );
+});
 
-{
-  const node1 = {
-    type: 'Program',
-    body: [],
-    sourceType: 'script',
-  };
-  const location2 = {
-    __proto__: null,
-    shouldBeInstrumented(...args) {
-      Assert.equal(this, location2);
-      Assert.deepEqual(args, []);
-      return false;
-    },
-  };
-  const location1 = {
-    __proto__: null,
-    extend(...args) {
-      Assert.equal(this, location1);
-      Assert.deepEqual(args, ['Program', node1]);
-      return location2;
-    },
-  };
-  assignVisitorObject('Program', {
-    Program: () => Assert.fail(),
-  });
-  const result = visit('Program', node1, location1);
-  Assert.equal(getResultNode(result), node1);
-  Assert.deepEqual(getResultEntities(result), []);
-}
+//
+//
+//
+// ////////////////////////////////
+// // shouldBeInstrumented: true //
+// ////////////////////////////////
+//
+// {
+//   input = {
+//     type: 'Identifier',
+//     name: "x"
+//   };
+//   const node2 = {
+//     type: 'Identifier',
+//     name: "y"
+//   };
+//   setVisitor("Program", (...args) => {
+//     Assert.deeEqual(args, [node1, {location2, }]);
+//     return [];
+//   }, (...args) => {
+//
+//   });
+//
+//
+//
+//   assignVisitorObject('Program', {
+//     Program: (...args) => {
+//       Assert.deepEqual(args, [node1, location2]);
+//       return combineResult((node, location) => node2, args[0], args[1]);
+//     },
+//   });
+//   const result = visit('Program', node1, location1);
+//   Assert.equal(getResultNode(result), node2);
+//   Assert.deepEqual(getResultEntities(result), []);
+// }
+//
+// /////////////////////////////////
+// // shouldBeInstrumented: false //
+// /////////////////////////////////
+//
+// {
+//   const node1 = {
+//     type: 'Program',
+//     body: [],
+//     sourceType: 'script',
+//   };
+//   const location2 = {
+//     __proto__: null,
+//     shouldBeInstrumented(...args) {
+//       Assert.equal(this, location2);
+//       Assert.deepEqual(args, []);
+//       return false;
+//     },
+//   };
+//   const location1 = {
+//     __proto__: null,
+//     extend(...args) {
+//       Assert.equal(this, location1);
+//       Assert.deepEqual(args, ['Program', node1]);
+//       return location2;
+//     },
+//   };
+//   assignVisitorObject('Program', {
+//     Program: () => Assert.fail(),
+//   });
+//   const result = visit('Program', node1, location1);
+//   Assert.equal(getResultNode(result), node1);
+//   Assert.deepEqual(getResultEntities(result), []);
+// }
