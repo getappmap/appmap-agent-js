465a918b7d688d4a12c225a1235ac6f716e9b463
Laurent Christophe <lachrist@vub.ac.be>
2021-04-02 16:39:13 +0200

    feat: config system is now more flexible
    The agent can now be configured both by a config file (which can extends another config file) and by
    environment variables (which can point to a config file)
    
    BREAKING CHANGE: Some old environment variables no longer work

--- test/unit/lib/server/inline.mjs
+++ test/unit/lib/server/inline.mjs
@@ -1,66 +1,101 @@
 import { strict as Assert } from 'assert';
 import * as FileSystem from 'fs';
-import inline from '../../../../lib/server/inline.mjs';
+import makeChannel from '../../../../lib/server/inline.mjs';
 
 const path = 'tmp/test/target.js';
 
-FileSystem.writeFileSync(path, `process.argv.concat(["qux"]);`, 'utf8');
+// FileSystem.writeFileSync(path, `process.argv.concat(["qux"]);`, 'utf8');
 
-Assert.deepEqual(
-  main({
-    _: [path, 'foo', 'bar'],
-  }),
-  ['node', path, 'foo', 'bar', 'qux'],
-);
+const channel = makeChannel({
+  APPMAP_OUTPUT_DIR: "tmp/appmap",
+  APPMAP_GIT_DIR:
+  APPMAP: "true",
+  APPMAP_CONFIG: "tmp/test/appmap.yml"
+});
 
-Assert.deepEqual(
-  main({
-    channel: 'foo',
-  }),
-  undefined,
-);
+// Assert.equal(
+//   channel.instrumentScript(`123;`, `filename.js`),
 
 
-
-
-import { strict as Assert } from 'assert';
-import { load } from '../../../__fixture__.mjs';
-
-load('src/es2015/node/send/local.js');
-
-const trace = [];
-
-global.APPMAP_GLOBAL_APPMAP_OBJECT = {
-  setEngine(...args) {
-    Assert.equal(this, global.APPMAP_GLOBAL_APPMAP_OBJECT);
-    trace.push(['engine', ...args]);
-  },
-  addEvent(...args) {
-    Assert.equal(this, global.APPMAP_GLOBAL_APPMAP_OBJECT);
-    trace.push(['event', ...args]);
-  },
-  archive(...args) {
-    Assert.equal(this, global.APPMAP_GLOBAL_APPMAP_OBJECT);
-    trace.push(['archive', ...args]);
-  },
-};
+channel.initialize({
+  pid: 123,
+  engine: "foo@bar",
+  name: "main",
+  prefix: "qux"
+});
 
 Assert.equal(
-  APPMAP_GLOBAL_SEND('engine', {
-    name: 'engine-name',
-    version: 'engine-version',
-  }),
-  true,
-);
+  channel.instrumentScript(`const  o1  =  {};`, `filename.js`),
+  `const o1 = {};`);
 
-Assert.equal(APPMAP_GLOBAL_SEND('event', 'event-data'), true);
+channel.instrumentModule(`const  o2  =  {};`, `filename.mjs`, {
+  resolve: (...args) => {
+    Assert.deepEqual(args, [`const o2 = {};`]);
+  },
+  reject: () => { Assert.fail() }
+});
 
-Assert.equal(APPMAP_GLOBAL_SEND('archive', 'archive-data'), true);
+channel.emit("event");
 
-Assert.equal(APPMAP_GLOBAL_SEND('foo', 'bar'), false);
+chanel.terminate("reason");
 
-Assert.deepEqual(trace, [
-  ['engine', 'engine-name', 'engine-version'],
-  ['event', 'event-data'],
-  ['archive', 'archive-data'],
-]);
+const json = JSON.parse(Fs.readFileSync(`tmp/appmap/main.appmap.json`, 'utf8'));
+
+// Assert.deepEqual(
+//   main({
+//     _: [path, 'foo', 'bar'],
+//   }),
+//   ['node', path, 'foo', 'bar', 'qux'],
+// );
+//
+// Assert.deepEqual(
+//   main({
+//     channel: 'foo',
+//   }),
+//   undefined,
+// );
+//
+//
+//
+//
+// import { strict as Assert } from 'assert';
+// import { load } from '../../../__fixture__.mjs';
+//
+// load('src/es2015/node/send/local.js');
+//
+// const trace = [];
+//
+// global.APPMAP_GLOBAL_APPMAP_OBJECT = {
+//   setEngine(...args) {
+//     Assert.equal(this, global.APPMAP_GLOBAL_APPMAP_OBJECT);
+//     trace.push(['engine', ...args]);
+//   },
+//   addEvent(...args) {
+//     Assert.equal(this, global.APPMAP_GLOBAL_APPMAP_OBJECT);
+//     trace.push(['event', ...args]);
+//   },
+//   archive(...args) {
+//     Assert.equal(this, global.APPMAP_GLOBAL_APPMAP_OBJECT);
+//     trace.push(['archive', ...args]);
+//   },
+// };
+//
+// Assert.equal(
+//   APPMAP_GLOBAL_SEND('engine', {
+//     name: 'engine-name',
+//     version: 'engine-version',
+//   }),
+//   true,
+// );
+//
+// Assert.equal(APPMAP_GLOBAL_SEND('event', 'event-data'), true);
+//
+// Assert.equal(APPMAP_GLOBAL_SEND('archive', 'archive-data'), true);
+//
+// Assert.equal(APPMAP_GLOBAL_SEND('foo', 'bar'), false);
+//
+// Assert.deepEqual(trace, [
+//   ['engine', 'engine-name', 'engine-version'],
+//   ['event', 'event-data'],
+//   ['archive', 'archive-data'],
+// ]);
