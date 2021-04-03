1425a2377c71bb119818dee8099d3a453617769c
Laurent Christophe <lachrist@vub.ac.be>
2021-04-03 11:13:28 +0200

    test: test logger
    Simplify the logger because changing process.env.NODE_DEBUG does not affect creating new logging
    function with util.debuglog

--- test/unit/lib/server/config.mjs
+++ test/unit/lib/server/config.mjs
@@ -2,163 +2,187 @@ import * as Fs from 'fs';
 import { strict as Assert } from 'assert';
 import Yaml from 'yaml';
 import * as Logger from '../../../../lib/server/logger.mjs';
-import Config from '../../../../lib/server/config.mjs';
+import { getDefaultConfig } from '../../../../lib/server/config.mjs';
 
-Logger.reloadGlobalLevel('CRITICAL');
+const config = getDefaultConfig();
 
-const inputString = `#comment
-name: appname
-packages:
-  - null
-  - foo: bar
-  - path: null
-  - path: package1
-  - path: package2
-    shallow: true
-  - path: package3
-    shallow: false
-  - path: package4
-    shallow: null
-exclude:
-  - Class
-  - Class#instanceMethod
-  - Class.classMethod
-  - null
-`;
+// enabled: DEFAULT_ENABLED,
+// app_name: DEFAULT_APP_NAME,
+// map_name: DEFAULT_MAP_NAME,
+// git_dir: DEFAULT_GIT_DIR,
+// language_version: DEFAULT_LANGUAGE_VERSION,
+// escape_prefix: DEFAULT_ESCAPE_PREFIX,
+// output_dir: DEFAULT_OUTPUT_DIR,
+// packages: [],
+// exclude: [],
 
-const inputObject = Yaml.parse(inputString);
+Assert.deepEqual(config.extendWithEnv({APPMAP: "TruE"}).isEnabled(), true);
+Assert.deepEqual(config.extendWithEnv({APPMAP: "FalsE"}).isEnabled(), false);
+Assert.deepEqual(config.extendWithEnv({APPMAP: "foo"}).isEnabled(), false);
 
-const makeInvalid = (path, key) => {
-  const object = { ...inputObject };
-  object[key] = null;
-  Fs.writeFileSync(path, Yaml.stringify(object), 'utf8');
-  return { APPMAP_CONFIG: path };
-};
+Assert.deepEqual(config.extendWithJson({enabled: true}).isEnabled(), true);
+Assert.deepEqual(config.extendWithJson({enabled: false}).isEnabled(), true);
+Assert.deepEqual(config.extendWithJson({enabled: "foo"}).isEnabled(), true);
 
-const makeMissing = (path, key) => {
-  const object = { ...inputObject };
-  delete object[key];
-  Fs.writeFileSync(path, Yaml.stringify(object), 'utf8');
-  return { APPMAP_CONFIG: path };
-};
 
-Fs.writeFileSync('tmp/test/appmap.yml', inputString, 'utf8');
+// Logger.reloadGlobalLevel('CRITICAL');
 
-const prototype = {
-  enabled: false,
-  outdir: 'tmp/appmap/',
-  appname: 'appname',
-  exclusions: ['Class', 'Class#instanceMethod', 'Class.classMethod'],
-  packages: [
-    { path: 'package1', depth: Infinity },
-    { path: 'package2', depth: 1 },
-    { path: 'package3', depth: Infinity },
-    { path: 'package4', depth: Infinity },
-  ],
-};
+// const inputString = `
+// #comment
+// name: appname
+// packages:
+//   - null
+//   - foo: bar
+//   - path: null
+//   - path: package1
+//   - path: package2
+//     shallow: true
+//   - path: package3
+//     shallow: false
+//   - path: package4
+//     shallow: null
+// exclude:
+//   - Class
+//   - Class#instanceMethod
+//   - Class.classMethod
+//   - null
+// `;
 
-const makeFresh = (array, element) => {
-  let index = 0;
-  while (array.includes(`${element}${String(index)}`)) {
-    index += 1;
-  }
-  return `${element}${String(index)}`;
-};
 
-const checkConfig = (config, expected) => {
-  Assert.equal(config.getOutputDir(), expected.outdir);
-  Assert.equal(config.getAppName(), expected.appname);
-  expected.exclusions.forEach((exclusion) => {
-    Assert.equal(config.isExcluded('package', exclusion), true);
-  });
-  Assert.equal(
-    config.isExcluded('package', makeFresh(expected.exclusions, 'Class')),
-    false,
-  );
-  expected.packages.forEach(({ path, depth }) => {
-    Assert.equal(
-      config.getInstrumentationDepth(path),
-      expected.enabled ? depth : 0,
-    );
-  });
-  Assert.equal(
-    config.getInstrumentationDepth(makeFresh(expected.packages, 'package')),
-    0,
-  );
-};
 
-[{ APPMAP: 'true' }, { APPMAP: 'TRUE' }].forEach((env) => {
-  checkConfig(
-    new Config({
-      ...env,
-      APPMAP_CONFIG: 'tmp/test/appmap.yml',
-      APPMAP_OUTPUT_DIR: 'appmap/output/dir/',
-    }),
-    {
-      __proto__: prototype,
-      outdir: 'appmap/output/dir/',
-      enabled: true,
-    },
-  );
-});
-
-process.chdir('tmp/test/');
-[{ APPMAP: 'false' }, { APPMAP: 'FALSE' }, { APPMAP: 'foobar' }, {}].forEach(
-  (env) => {
-    checkConfig(new Config(env), prototype);
-  },
-);
-process.chdir('../../');
-
-[
-  {
-    APPMAP_CONFIG: 'tmp/test/missing.yml',
-  },
-  {
-    APPMAP_CONFIG:
-      (Fs.writeFileSync('tmp/test/unparsable.yml', '- * -', 'utf8'),
-      'tmp/test/unparsable.yml'),
-  },
-  {
-    APPMAP_CONFIG:
-      (Fs.writeFileSync('tmp/test/invalid.yml', 'null', 'utf8'),
-      'tmp/test/invalid.yml'),
-  },
-].forEach((env) => {
-  checkConfig(new Config(env), {
-    __proto__: prototype,
-    appname: 'unknown',
-    packages: [],
-    exclusions: [],
-  });
-});
-
-[
-  makeInvalid('tmp/test/invalid-name.yml', 'name'),
-  makeMissing('tmp/test/missing-name.yml', 'name'),
-].forEach((env) => {
-  checkConfig(new Config(env), {
-    __proto__: prototype,
-    appname: 'unknown',
-  });
-});
-
-[
-  makeInvalid('tmp/test/invalid-packages.yml', 'packages'),
-  makeMissing('tmp/test/missing-packages.yml', 'packages'),
-].forEach((env) => {
-  checkConfig(new Config(env), {
-    __proto__: prototype,
-    packages: [],
-  });
-});
-
-[
-  makeInvalid('tmp/test/invalid-exclude.yml', 'exclude'),
-  makeMissing('tmp/test/missing-exclude.yml', 'exclude'),
-].forEach((env) => {
-  checkConfig(new Config(env), {
-    __proto__: prototype,
-    exclusions: [],
-  });
-});
+// const inputObject = Yaml.parse(inputString);
+//
+// const makeInvalid = (path, key) => {
+//   const object = { ...inputObject };
+//   object[key] = null;
+//   Fs.writeFileSync(path, Yaml.stringify(object), 'utf8');
+//   return { APPMAP_CONFIG: path };
+// };
+//
+// const makeMissing = (path, key) => {
+//   const object = { ...inputObject };
+//   delete object[key];
+//   Fs.writeFileSync(path, Yaml.stringify(object), 'utf8');
+//   return { APPMAP_CONFIG: path };
+// };
+//
+// Fs.writeFileSync('tmp/test/appmap.yml', inputString, 'utf8');
+//
+// const prototype = {
+//   enabled: false,
+//   outdir: 'tmp/appmap/',
+//   appname: 'appname',
+//   exclusions: ['Class', 'Class#instanceMethod', 'Class.classMethod'],
+//   packages: [
+//     { path: 'package1', depth: Infinity },
+//     { path: 'package2', depth: 1 },
+//     { path: 'package3', depth: Infinity },
+//     { path: 'package4', depth: Infinity },
+//   ],
+// };
+//
+// const makeFresh = (array, element) => {
+//   let index = 0;
+//   while (array.includes(`${element}${String(index)}`)) {
+//     index += 1;
+//   }
+//   return `${element}${String(index)}`;
+// };
+//
+// const checkConfig = (config, expected) => {
+//   Assert.equal(config.getOutputDir(), expected.outdir);
+//   Assert.equal(config.getAppName(), expected.appname);
+//   expected.exclusions.forEach((exclusion) => {
+//     Assert.equal(config.isExcluded('package', exclusion), true);
+//   });
+//   Assert.equal(
+//     config.isExcluded('package', makeFresh(expected.exclusions, 'Class')),
+//     false,
+//   );
+//   expected.packages.forEach(({ path, depth }) => {
+//     Assert.equal(
+//       config.getInstrumentationDepth(path),
+//       expected.enabled ? depth : 0,
+//     );
+//   });
+//   Assert.equal(
+//     config.getInstrumentationDepth(makeFresh(expected.packages, 'package')),
+//     0,
+//   );
+// };
+//
+// [{ APPMAP: 'true' }, { APPMAP: 'TRUE' }].forEach((env) => {
+//   checkConfig(
+//     new Config({
+//       ...env,
+//       APPMAP_CONFIG: 'tmp/test/appmap.yml',
+//       APPMAP_OUTPUT_DIR: 'appmap/output/dir/',
+//     }),
+//     {
+//       __proto__: prototype,
+//       outdir: 'appmap/output/dir/',
+//       enabled: true,
+//     },
+//   );
+// });
+//
+// process.chdir('tmp/test/');
+// [{ APPMAP: 'false' }, { APPMAP: 'FALSE' }, { APPMAP: 'foobar' }, {}].forEach(
+//   (env) => {
+//     checkConfig(new Config(env), prototype);
+//   },
+// );
+// process.chdir('../../');
+//
+// [
+//   {
+//     APPMAP_CONFIG: 'tmp/test/missing.yml',
+//   },
+//   {
+//     APPMAP_CONFIG:
+//       (Fs.writeFileSync('tmp/test/unparsable.yml', '- * -', 'utf8'),
+//       'tmp/test/unparsable.yml'),
+//   },
+//   {
+//     APPMAP_CONFIG:
+//       (Fs.writeFileSync('tmp/test/invalid.yml', 'null', 'utf8'),
+//       'tmp/test/invalid.yml'),
+//   },
+// ].forEach((env) => {
+//   checkConfig(new Config(env), {
+//     __proto__: prototype,
+//     appname: 'unknown',
+//     packages: [],
+//     exclusions: [],
+//   });
+// });
+//
+// [
+//   makeInvalid('tmp/test/invalid-name.yml', 'name'),
+//   makeMissing('tmp/test/missing-name.yml', 'name'),
+// ].forEach((env) => {
+//   checkConfig(new Config(env), {
+//     __proto__: prototype,
+//     appname: 'unknown',
+//   });
+// });
+//
+// [
+//   makeInvalid('tmp/test/invalid-packages.yml', 'packages'),
+//   makeMissing('tmp/test/missing-packages.yml', 'packages'),
+// ].forEach((env) => {
+//   checkConfig(new Config(env), {
+//     __proto__: prototype,
+//     packages: [],
+//   });
+// });
+//
+// [
+//   makeInvalid('tmp/test/invalid-exclude.yml', 'exclude'),
+//   makeMissing('tmp/test/missing-exclude.yml', 'exclude'),
+// ].forEach((env) => {
+//   checkConfig(new Config(env), {
+//     __proto__: prototype,
+//     exclusions: [],
+//   });
+// });
--- test/unit/lib/server/logger.mjs
+++ test/unit/lib/server/logger.mjs
@@ -1,40 +1,6 @@
 import { strict as Assert } from 'assert';
-import * as Logger from '../../../../lib/server/logger.mjs';
+import logger from '../../../../lib/server/logger.mjs';
 
-const LoggerDefault = Logger.default;
-
-class Writable {
-  constructor() {
-    this.content = '';
-  }
-  write(message, encoding) {
-    Assert.equal(encoding, 'utf8');
-    this.content += message;
-  }
-  getContent() {
-    return this.content;
-  }
-}
-
-const writable1 = new Writable();
-const logger1 = new LoggerDefault('name1', Logger.DEBUG, writable1);
-let content1 = '';
-Assert.equal(logger1.getLevel(), Logger.DEBUG);
-['debug', 'info', 'warning', 'error', 'critical'].forEach((name) => {
-  Assert.equal(logger1[name](name), undefined);
-  content1 += `${name.toUpperCase()} name1 >> ${name}${'\n'}`;
-});
-Assert.equal(writable1.getContent(), content1);
-
-const writable2 = new Writable();
-Logger.reloadGlobalLevel('CRITICAL');
-const logger2 = new LoggerDefault('name2', undefined, writable2);
-Assert.equal(logger2.getLevel(), Logger.CRITICAL);
-['debug', 'info', 'warning', 'error', 'critical'].forEach((name) => {
-  Assert.equal(logger2[name](name), undefined);
-});
-Assert.equal(writable2.getContent(), 'CRITICAL name2 >> critical\n');
-Logger.reloadGlobalLevel('ERROR');
-Assert.equal(logger2.getLevel(), Logger.ERROR);
-Logger.reloadGlobalLevel('FOO');
-Assert.equal(logger2.getLevel(), Logger.ERROR);
+logger.error("error");
+logger.warning("warning");
+logger.info("info");
