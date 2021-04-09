266fe271bf56d99a8a8bca3a93a82f792472b84d
Laurent Christophe <lachrist@vub.ac.be>
2021-04-09 16:00:27 +0200

    fix: run eslint and prettier

--- bin/bin-inline.mjs
+++ bin/bin-inline.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 import minimist from 'minimist';
-import main from '../lib/server/inline-main.mjs';
+import main from '../lib/server/inline/main.mjs';
 
 main(minimist(process.argv.slice(2)), 'inherit');
