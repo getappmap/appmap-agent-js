8f3c253f71d321100541c9ccbc4356f8788e7473
Laurent Christophe <lachrist@vub.ac.be>
2021-08-03 23:34:51 +0200

    test: test node entry point

---
+++ main/loader.mjs
@@ -0,0 +1,11 @@
+/* globals APPMAP_TRANSFORM_SOURCE */
+import {runInThisContext} from "vm";
+
+runInThisContext("let APPMAP_TRANSFORM_SOURCE = null;");
+
+export const transformSource = (content, context, transformSource) => {
+  if (APPMAP_TRANSFORM_SOURCE !== null) {
+    return APPMAP_TRANSFORM_SOURCE(content, context, transformSource);
+  }
+  transformSource(content, context, transformSource);
+};
