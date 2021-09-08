c6db2e471197de4d6643b00b9da8f5283bc878a5
Laurent Christophe <lachrist@vub.ac.be>
2021-09-08 13:27:19 +0200

    feat: new event data compilation

--- components/trace/appmap/event.mjs
+++ components/trace/appmap/event.mjs
@@ -1,247 +1,231 @@
+/* globals URL, URLSearchParams */
+const _URL = URL;
+const _URLSearchParams = URLSearchParams;
+const _String = String;
 import Classmap from "./classmap.mjs";
-import EventData from "./event-data.mjs";
 
-const _Map = Map;
+const { entries: toEntries } = Object;
+const { from: arrayFrom } = Array;
 
 export default (dependencies) => {
   const {
-    util: { assert },
+    util: { assert, coalesceCaseInsensitive, zip },
   } = dependencies;
-
   const { getClassmapClosure } = Classmap(dependencies);
 
-  const { compileBeforeEventData, compileAfterEventData } =
-    EventData(dependencies);
+  const parseURL = (url, headers) =>
+    new _URL(
+      url[0] === "/"
+        ? `http://${coalesceCaseInsensitive(
+            headers,
+            "host",
+            "localhost",
+          )}${url}`
+        : url,
+    );
 
-  const placeholders = new _Map([
-    // bundle //
-    ["begin/bundle", { type: "bundle" }],
-    ["after/bundle", { type: "bundle" }],
-    [
-      "begin/apply",
-      {
-        type: "apply",
-        function: null,
-        this: {
-          type: "string",
-          print: "MANUFACTURED APPMAP THIS VALUE",
-        },
-        arguments: [],
-      },
-    ],
-    [
-      "end/apply",
-      {
-        type: "apply",
-        error: null,
-        result: {
-          type: "string",
-          print: "MANUFACTURED APPMAP RETURN VALUE",
-        },
-      },
-    ],
-    [
-      "begin/response",
-      {
-        type: "response",
-        protocol: "HTTP/1.1",
-        method: "GET",
-        path: "/MANUFACTURED/APPMAP/REQUEST",
-        headers: {},
-      },
-    ],
-    [
-      "end/response",
-      {
-        type: "response",
-        status: 200,
-        message: "MANUFACTURED-APPMAP-RESPONSE",
-        headers: {},
-      },
-    ],
-    // jump //
-    ["before/jump", { type: "jump" }],
-    ["after/jump", { type: "jump" }],
-    [
-      "before/query",
-      {
-        type: "query",
-        database: "MANUFACTURED_APPMAP_DATABASE",
-        version: null,
-        sql: "SELECT * FROM MANUFACTURED_APPMAP_TABLE;",
-        parameters: [],
-      },
-    ],
-    [
-      "after/query",
-      {
-        type: "query",
-        error: { type: "null", print: "null" },
-      },
-    ],
-    [
-      "before/request",
-      {
-        type: "request",
-        protocol: "HTTP/1.1",
-        method: "GET",
-        path: "/MANUFACTURED/APPMAP/REQUEST",
-        headers: {},
-      },
-    ],
-    [
-      "after/request",
-      {
-        type: "request",
-        status: 200,
-        message: "MANUFACTURED-APPMAP-RESPONSE",
-        headers: {},
-      },
-    ],
-  ]);
-
-  const checkFinish = (frame) => {
-    const {type} = frame;
-    if (type === "jump") {
-      const {after} = frame;
-      assert(after !== null, "unfinished jump");
-    } else {
-      assert(type === "bundle", "invalid frame");
-      const {end} = frame;
-      assert(end !== null, "unfinished bundle");
-    }
+  const placeholder = {
+    link: {
+      defined_class: "MANUFACTURED_APPMAP_CLASS",
+      lineno: 0,
+      method_id: "MANUFACTURED_APPMAP_METHOD",
+      static: false,
+      path: "MANUFACTURED_APPMAP_FILE.js",
+    },
+    parameters: [],
   };
 
-  const orderByFrame = (events) => {
-    const root = { type:"bundle", begin: null, between: [], end: null };
-    const stack = [];
-    let current = root;
-    const map = new _Map();
-    for (const event of events) {
-      const { type, index } = event;
-      if (type === "begin") {
-        const frame = {
-          type: "bundle",
-          begin: event,
-          between: [],
-          end: null,
-        };
-        map.set(index, frame);
-        stack.push(current);
-        current = frame;
-      } else if (type === "end" || type === "before") {
-        const {begin, end, between} = current;
-        const {index:begin_index} = begin;
-        assert(begin_index === index, "bundle index mismatch");
-        assert(end === null, "bundle frame has already ended");
-        if (type === "end") {
-          const {data:{type:begin_type}} = begin;
-          assert(type === begin_type, "bundle type mismatch");
-          between.map(checkFinish);
-          current.end = event;
-        } else {
-          between.push({
-            type: "jump",
-            before: event,
-            after: null
-          });
-        }
-        const {length} = stack;
-        assert(length > 0, "empty stack");
-        current = stack.pop();
-      } else if (type === "after") {
-        assert(map.has(index), "missing frame for after event");
-        stack.push(current);
-        current = map.get(index);
-        const {between} = current;
-        const {length} = between;
-        assert(length > 0, "after event expected something in between");
-        const last = between[length - 1];
-        const {type:last_type} = last;
-        assert(last_type === "jump", "expected a jump");
-        const {before, after} = last;
-        const {data:{type:before_type}} = before;
-        assert(type === before_type, "jump type mismatch");
-        assert(after === null, "jump has already been used");
-        last.after = event;
-      } else {
-        assert(false, "invalid event type");
-      }
-    }
-    return root.between;
-  };
+  const isFirstColon = ([string]) => string.startsWith(":");
 
-  const createCallEvent = ({index, data}, event2, classmap) => ({
-    id: 2 * index,
-    event: "call",
-    thread_id: 0,
-    ...compileBeforeEventData(data, classmap),
-  });
+  const compileSearchMessage = (search) =>
+    arrayFrom(new _URLSearchParams(search).entries());
 
-  const createReturnEvent = ({type:type1, index, time, data:{type:type2}}, event2, classmap) => ({
-    id: 2 * index + 1,
-    event: "return",
-    thread_id: 0,
-    parent_id: 2 * index,
-    elapsed: event2 === null ? 0 : event2.time - time,
-    ...compileAfterEventData(
-      event2 === null ? placeholders.get(`${type1}/${type2}`) : event2.data,
-      classmap
-    ),
-  });
-
-  const isEmptyBundle = ({data:{type}}) => type === "bundle";
-
-  const isEmptyJump = ({data:{type}}) => type === "jump";
-
-  const isShallow = ({data}, classmap) => {
-    const {type} = data;
+  const compileBeginEventData = (data, classmap) => {
+    const { type } = data;
     if (type === "apply") {
-      const {route} = data;
-      const {
-        file: { shallow },
-      } = getClassmapClosure(classmap, route);
-      return shallow;
+      const { function: route, this: _this, arguments: _arguments } = data;
+      const { link, parameters } =
+        route === null ? placeholder : getClassmapClosure(classmap, route);
+      return {
+        ...link,
+        receiver: compileParameterSerial(["this", _this]),
+        parameters: zip(parameters, _arguments).map(compileParameterSerial),
+      };
     }
-    return false;
+    if (type === "response") {
+      const { protocol, method, url, headers, route } = data;
+      const { pathname, search } = parseURL(url, headers);
+      return {
+        http_server_request: {
+          headers,
+          authorization: coalesceCaseInsensitive(
+            headers,
+            "authorization",
+            null,
+          ),
+          mime_type: coalesceCaseInsensitive(headers, "content-type", null),
+          request_method: method,
+          path_info: pathname,
+          normalized_path_info: route,
+          protocol,
+        },
+        message: [
+          ...(route === null
+            ? []
+            : zip(route.split("/"), pathname.split("/")).filter(isFirstColon)),
+          ...compileSearchMessage(search),
+        ].map(compileParameterPrimitive),
+      };
+    }
+    /* c8 ignore start */
+    assert(false, "invalid begin event type");
+    /* c8 ignore stop */
   };
 
-  const compileTrace = (frames, classmap) => {
-    const generateCompileFrame = (shallow) => (frame) => {
-      const {type} = frame;
-      if (type === "jump") {
-        const {before, after} = frame;
-        if (isEmptyJump(before)) {
-          return [];
-        }
-        return [
-          createCallEvent(before, after, classmap),
-          createReturnEvent(before, after, classmap),
-        ];
-      }
-      assert(type === "bundle", "invalid frame");
-      const {begin, between, end} = frame;
-      const next_shallow = isShallow(begin, classmap);
-      const trace = between.flatMap(next_shallow ? compileShallowFrame : compileRegularFrame);
-      if ((shallow && next_shallow) || isEmptyBundle(begin)) {
-        return trace;
-      }
-      return [
-        createCallEvent(begin, end, classmap),
-        ... trace,
-        createReturnEvent(begin, end, classmap),
-      ];
+  const compileBeforeEventData = (data, classmap) => {
+    const { type } = data;
+    if (type === "query") {
+      const { database, version, sql, parameters } = data;
+      return {
+        sql_query: {
+          database_type: database,
+          server_version: version,
+          sql,
+          explain_sql: null,
+        },
+        message: toEntries(parameters).map(compileParameterSerial),
+      };
+    }
+    if (type === "request") {
+      const { method, url, headers } = data;
+      const { origin, pathname, search } = parseURL(url, headers);
+      return {
+        http_client_request: {
+          request_method: method,
+          url: `${origin}${pathname}`,
+          headers,
+        },
+        message: compileSearchMessage(search).map(compileParameterPrimitive),
+      };
+    }
+    /* c8 ignore start */
+    assert(false, "invalid before event type");
+    /* c8 ignore stop */
+  };
+
+  const compileEndEventData = (data, classmap) => {
+    const { type } = data;
+    if (type === "apply") {
+      const { result, error } = data;
+      return {
+        return_value:
+          result === null ? null : compileParameterSerial(["return", result]),
+        exceptions: error === null ? null : [compileExceptionSerial(error)],
+      };
+    }
+    if (type === "response") {
+      const { status, headers } = data;
+      return {
+        http_server_response: {
+          status_code: status,
+          mime_type: coalesceCaseInsensitive(headers, "content-type", null),
+        },
+      };
+    }
+    /* c8 ignore start */
+    assert(false, "invalid end event type");
+    /* c8 ignore stop */
+  };
+
+  const compileAfterEventData = (data, classmap) => {
+    const { type } = data;
+    if (type === "query") {
+      return {};
+    }
+    if (type === "request") {
+      const { status, headers } = data;
+      return {
+        http_client_response: {
+          status_code: status,
+          mime_type: coalesceCaseInsensitive(headers, "content-type", null),
+          headers,
+        },
+      };
+    }
+    /* c8 ignore start */
+    assert(false, "invalid after event type");
+    /* c8 ignore stop */
+  };
+
+  const compileParameterPrimitive = ([name, primitive]) => ({
+    name,
+    class: typeof primitive,
+    object_id: null,
+    value: _String(primitive),
+  });
+
+  const compileParameterSerial = ([name, serial]) => {
+    const {
+      type,
+      constructor: _constructor,
+      index,
+      truncated,
+      print,
+    } = {
+      type: null,
+      constructor: null,
+      index: null,
+      truncated: false,
+      print: null,
+      ...serial,
     };
-    const compileShallowFrame = generateCompileFrame(true);
-    const compileRegularFrame = generateCompileFrame(false);
-    return frames.flatMap(compileRegularFrame);
-  }
+    return {
+      name,
+      class: _constructor === null ? type : _constructor,
+      object_id: index,
+      value: truncated ? `${print} ...` : print,
+    };
+  };
+
+  const compileExceptionSerial = (serial) => {
+    const {
+      type,
+      constructor: _constructor,
+      index,
+      specific,
+    } = {
+      type: null,
+      constructor: null,
+      index: null,
+      specific: null,
+      ...serial,
+    };
+    let stack = null;
+    let message = null;
+    if (specific !== null) {
+      const { type } = specific;
+      if (type === "error") {
+        ({ stack, message } = specific);
+      }
+    }
+    return {
+      class: _constructor === null ? type : _constructor,
+      message,
+      object_id: index,
+      // TODO: extract path from stack
+      path: stack,
+      // TODO: extract line number from stack
+      lineno: null,
+    };
+  };
 
   return {
-    compileEventTrace: (events, classmap) => compileTrace(
-      orderByFrame(events),
-      classmap,
-    ),
+    compileParameterPrimitive, // export for testing
+    compileParameterSerial, // export for testing
+    compileExceptionSerial, // export for testing
+    compileBeginEventData,
+    compileEndEventData,
+    compileBeforeEventData,
+    compileAfterEventData,
   };
-
 };
--- components/trace/appmap/event.test.mjs
+++ components/trace/appmap/event.test.mjs
@@ -11,156 +11,427 @@ Error.stackTraceLimit = Infinity;
 const {
   deepEqual: assertDeepEqual,
   // throws: assertThrows,
-  equal: assertEqual,
+  // equal: assertEqual,
 } = Assert;
 
 const testAsync = async () => {
   const dependencies = await buildTestDependenciesAsync(import.meta.url);
-  const { createConfiguration } = await buildTestComponentAsync(
-    "configuration",
-  );
+  const { createConfiguration, extendConfiguration } =
+    await buildTestComponentAsync("configuration", "test");
   const { createClassmap, addClassmapFile } = Classmap(dependencies);
-  const { compileEventTrace } = Event(dependencies);
-  const classmap = createClassmap(createConfiguration("/cwd"));
-  const default_before_event = {
-    type: "before",
-    time: 0,
-    index: null,
-    data: { type: "test" },
-  };
-  const default_after_event = {
-    type: "after",
-    time: 0,
-    index: null,
-    data: { type: "test" },
-  };
-  const default_call_event = {
-    event: "call",
-    thread_id: 0,
-    id: null,
-  };
-  const default_return_event = {
-    event: "return",
-    thread_id: 0,
-    id: null,
-    elapsed: 0,
-    parent_id: null,
-  };
-  // sort return //
+  const {
+    compileBeginEventData,
+    compileEndEventData,
+    compileBeforeEventData,
+    compileAfterEventData,
+    compileParameterPrimitive,
+    compileExceptionSerial,
+    compileParameterSerial,
+  } = Event(dependencies);
+  const configuration = extendConfiguration(createConfiguration("/cwd"), {
+    "function-name-placeholder": "$",
+  });
+  const classmap = createClassmap(configuration);
+  addClassmapFile(classmap, {
+    index: 123,
+    path: "/cwd/filename.js",
+    type: "script",
+    code: "function f (x) {}",
+    exclude: [],
+  });
+
+  ////////////
+  // serial //
+  ////////////
+
+  // compileParameterSerial //
+
   assertDeepEqual(
-    compileEventTrace(
-      [
-        { ...default_before_event, index: 1 },
-        { ...default_before_event, index: 2 },
-        { ...default_after_event, index: 1 },
-        { ...default_after_event, index: 2 },
-      ],
-      classmap,
-    ),
-    [
-      { ...default_call_event, id: 2 },
-      { ...default_call_event, id: 4 },
-      { ...default_return_event, id: 5, parent_id: 4 },
-      { ...default_return_event, id: 3, parent_id: 2 },
-    ],
+    compileParameterSerial([
+      "name",
+      { type: "string", truncated: true, print: "print" },
+    ]),
+    {
+      name: "name",
+      object_id: null,
+      class: "string",
+      value: "print ...",
+    },
   );
-  // flip order //
+
   assertDeepEqual(
-    compileEventTrace(
-      [
-        { ...default_after_event, index: 1 },
-        { ...default_before_event, index: 2 },
-        { ...default_after_event, index: 2 },
-        { ...default_before_event, index: 1 },
-      ],
-      classmap,
-    ),
-    [
-      { ...default_call_event, id: 2 },
-      { ...default_call_event, id: 4 },
-      { ...default_return_event, id: 5, parent_id: 4 },
-      { ...default_return_event, id: 3, parent_id: 2 },
-    ],
-  );
-  // manufacture missing event //
-  assertDeepEqual(
-    compileEventTrace(
-      [
-        { ...default_after_event, index: 1 },
-        { ...default_before_event, index: 2 },
-      ],
-      classmap,
-    ),
-    [
-      { ...default_call_event, id: 2 },
-      { ...default_call_event, id: 4 },
-      { ...default_return_event, id: 5, parent_id: 4 },
-      { ...default_return_event, id: 3, parent_id: 2 },
-    ],
-  );
-  // collapseShallow //
-  {
-    const testCollapse = (shallow) => {
-      const classmap = createClassmap(createConfiguration("/cwd"));
-      addClassmapFile(classmap, {
+    compileParameterSerial([
+      "name",
+      {
         index: 123,
-        path: "/cwd/filename.js",
-        type: "script",
-        code: "function f (x) {}",
-        exclude: [],
-        shallow,
-      });
-      const default_before_apply_event = {
-        ...default_before_event,
-        data: {
-          type: "apply",
-          function: "123/body/0",
-          this: { type: "string", print: "print-this" },
-          arguments: [{ type: "string", print: "print-arg" }],
+        constructor: "constructor",
+        type: "object",
+        print: "print",
+      },
+    ]),
+    {
+      name: "name",
+      object_id: 123,
+      class: "constructor",
+      value: "print",
+    },
+  );
+
+  // compileParameterPrimitive //
+
+  assertDeepEqual(compileParameterPrimitive(["name", "primitive"]), {
+    name: "name",
+    object_id: null,
+    class: "string",
+    value: "primitive",
+  });
+
+  // compileExceptionSerial //
+
+  assertDeepEqual(
+    compileExceptionSerial({
+      index: 123,
+      constructor: "constructor",
+      truncated: false,
+      print: "print",
+      specific: { type: "error", stack: "stack", message: "message" },
+    }),
+    {
+      object_id: 123,
+      class: "constructor",
+      message: "message",
+      path: "stack",
+      lineno: null,
+    },
+  );
+
+  assertDeepEqual(
+    compileExceptionSerial({
+      type: "string",
+      print: "print",
+    }),
+    {
+      object_id: null,
+      class: "string",
+      message: null,
+      path: null,
+      lineno: null,
+    },
+  );
+
+  ///////////////////////////
+  // compileBeginEventData //
+  ///////////////////////////
+
+  // apply >> routing //
+  assertDeepEqual(
+    compileBeginEventData(
+      {
+        type: "apply",
+        function: "123/body/0",
+        this: { type: "string", print: "print-this" },
+        arguments: [{ type: "string", print: "print-arg" }],
+      },
+      classmap,
+    ),
+    {
+      defined_class: "f",
+      lineno: 1,
+      method_id: "$",
+      static: false,
+      path: "filename.js",
+      parameters: [
+        {
+          class: "string",
+          name: "x",
+          object_id: null,
+          value: "print-arg",
         },
-      };
-      const default_after_apply_event = {
-        ...default_after_event,
-        data: {
-          type: "apply",
-          error: null,
-          result: { type: "string", print: "print" },
+      ],
+      receiver: {
+        class: "string",
+        name: "this",
+        object_id: null,
+        value: "print-this",
+      },
+    },
+  );
+
+  // apply >> missing routing //
+  assertDeepEqual(
+    compileBeginEventData(
+      {
+        type: "apply",
+        function: null,
+        this: { type: "string", print: "print-this" },
+        arguments: [{ type: "string", print: "print-arg" }],
+      },
+      classmap,
+    ),
+    {
+      defined_class: "MANUFACTURED_APPMAP_CLASS",
+      lineno: 0,
+      method_id: "MANUFACTURED_APPMAP_METHOD",
+      static: false,
+      path: "MANUFACTURED_APPMAP_FILE.js",
+      parameters: [],
+      receiver: {
+        class: "string",
+        name: "this",
+        object_id: null,
+        value: "print-this",
+      },
+    },
+  );
+
+  // response >> message //
+  assertDeepEqual(
+    compileBeginEventData({
+      type: "response",
+      method: "GET",
+      protocol: "HTTP/1.1",
+      url: "http://host:8080/path/info?search=param#hash",
+      route: "/path/:info",
+      headers: {},
+    }),
+    {
+      http_server_request: {
+        request_method: "GET",
+        path_info: "/path/info",
+        normalized_path_info: "/path/:info",
+        protocol: "HTTP/1.1",
+        authorization: null,
+        mime_type: null,
+        headers: {},
+      },
+      message: [
+        {
+          name: ":info",
+          object_id: null,
+          class: "string",
+          value: "info",
         },
-      };
-      const { length } = compileEventTrace(
-        [
-          {
-            ...default_before_event,
-            index: 1,
-          },
-          {
-            ...default_before_apply_event,
-            index: 2,
-          },
-          {
-            ...default_before_apply_event,
-            index: 3,
-          },
-          {
-            ...default_after_apply_event,
-            index: 3,
-          },
-          {
-            ...default_after_apply_event,
-            index: 2,
-          },
-          {
-            ...default_after_event,
-            index: 1,
-          },
-        ],
-        classmap,
-      );
-      assertEqual(length, shallow ? 4 : 6);
-    };
-    testCollapse(false);
-    testCollapse(true);
-  }
+        {
+          name: "search",
+          object_id: null,
+          class: "string",
+          value: "param",
+        },
+      ],
+    },
+  );
+
+  // response >> headers //
+  assertDeepEqual(
+    compileBeginEventData({
+      type: "response",
+      method: "GET",
+      protocol: "HTTP/1.1",
+      url: "/path",
+      route: null,
+      headers: {
+        AUTHORIZATION: "authorization",
+        "CONTENT-TYPE": "content-type",
+      },
+    }),
+    {
+      http_server_request: {
+        request_method: "GET",
+        path_info: "/path",
+        normalized_path_info: null,
+        protocol: "HTTP/1.1",
+        authorization: "authorization",
+        mime_type: "content-type",
+        headers: {
+          AUTHORIZATION: "authorization",
+          "CONTENT-TYPE": "content-type",
+        },
+      },
+      message: [],
+    },
+  );
+
+  ////////////////////////////
+  // compileBeforeEventData //
+  ////////////////////////////
+
+  // query //
+  assertDeepEqual(
+    compileBeforeEventData({
+      type: "query",
+      database: "database",
+      version: "version",
+      sql: "sql",
+      parameters: [{ type: "string", print: "print" }],
+    }),
+    {
+      sql_query: {
+        database_type: "database",
+        server_version: "version",
+        sql: "sql",
+        explain_sql: null,
+      },
+      message: [
+        {
+          name: "0",
+          object_id: null,
+          class: "string",
+          value: "print",
+        },
+      ],
+    },
+  );
+
+  // request >> message //
+  assertDeepEqual(
+    compileBeforeEventData({
+      type: "request",
+      method: "GET",
+      protocol: "HTTP/1.1",
+      url: "http://host:8080/path?search=param#hash",
+      headers: {},
+    }),
+    {
+      http_client_request: {
+        request_method: "GET",
+        url: "http://host:8080/path",
+        headers: {},
+      },
+      message: [
+        {
+          name: "search",
+          object_id: null,
+          class: "string",
+          value: "param",
+        },
+      ],
+    },
+  );
+
+  // request >> headers //
+  assertDeepEqual(
+    compileBeforeEventData({
+      type: "request",
+      method: "GET",
+      protocol: "HTTP/1.1",
+      url: "/path",
+      headers: { HOST: "host:8080" },
+    }),
+    {
+      http_client_request: {
+        request_method: "GET",
+        url: "http://host:8080/path",
+        headers: { HOST: "host:8080" },
+      },
+      message: [],
+    },
+  );
+
+  /////////////////////
+  // compileEndEvent //
+  /////////////////////
+
+  // apply >> success //
+  assertDeepEqual(
+    compileEndEventData(
+      {
+        type: "apply",
+        error: null,
+        result: { type: "string", print: "print" },
+      },
+      classmap,
+    ),
+    {
+      exceptions: null,
+      return_value: {
+        class: "string",
+        name: "return",
+        object_id: null,
+        value: "print",
+      },
+    },
+  );
+
+  // apply >> failure //
+  assertDeepEqual(
+    compileEndEventData(
+      {
+        type: "apply",
+        error: {
+          index: 123,
+          constructor: "constructor",
+          print: "print",
+          specific: { type: "error", message: "message", stack: "stack" },
+        },
+        result: null,
+      },
+      classmap,
+    ),
+    {
+      exceptions: [
+        {
+          object_id: 123,
+          class: "constructor",
+          message: "message",
+          path: "stack",
+          lineno: null,
+        },
+      ],
+      return_value: null,
+    },
+  );
+
+  // response //
+  assertDeepEqual(
+    compileEndEventData(
+      {
+        type: "response",
+        status: 200,
+        headers: { "CONTENT-TYPE": "content-type" },
+      },
+      classmap,
+    ),
+    {
+      http_server_response: {
+        status_code: 200,
+        mime_type: "content-type",
+      },
+    },
+  );
+
+  ///////////////////////////
+  // compileAfterEventData //
+  ///////////////////////////
+
+  // query //
+  assertDeepEqual(
+    compileAfterEventData(
+      {
+        type: "query",
+      },
+      classmap,
+    ),
+    {},
+  );
+
+  // request //
+  assertDeepEqual(
+    compileAfterEventData(
+      {
+        type: "request",
+        status: 200,
+        headers: { "CONTENT-TYPE": "content-type" },
+      },
+      classmap,
+    ),
+    {
+      http_client_response: {
+        status_code: 200,
+        mime_type: "content-type",
+        headers: { "CONTENT-TYPE": "content-type" },
+      },
+    },
+  );
 };
 
 testAsync();
