3bb79b22563bc27cdbaffb738021d12dae1930c8
Laurent Christophe <lachrist@vub.ac.be>
2021-11-10 19:12:17 +0100

    feat: help and version in CLI

--- bin/bin.mjs
+++ bin/bin.mjs
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
 
-const methods = ["setup", "batch"];
+const routes = new Map(
+  [
+    ["setup", "../lib/node/setup.mjs"],
+    ["run", "../lib/node/batch.mjs"],
+    ["version", "../lib/node/version.mjs"],
+    ["help", "../lib/node/version.mjs"],
+  ],
+);
 
-let method = "batch";
+let route = "run";
 
-if (methods.includes(process.argv[2])) {
-  method = process.argv[2];
+if (routes.has(process.argv[2])) {
+  route = process.argv[2];
   process.argv = [process.argv[0], process.argv[1], ...process.argv.slice(3)];
 }
 
-if (method === "batch") {
-  import("../lib/node/batch.mjs");
-} else if (method === "setup") {
-  import("../lib/node/setup.mjs");
-} else {
-  throw new Error("This should never happen");
-}
+await import(routes.get(route));
