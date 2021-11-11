0bd7f74b1f1fd12e16ef89a2a062334606bd942e
Laurent Christophe <lachrist@vub.ac.be>
2021-11-11 08:47:50 +0100

    feat: support --help and --version aliases
    This might be subpar because -- prefix hints a named argument whereas in this case --help and --version are actually positional argument (respectively aliases for help and version).

--- bin/bin.mjs
+++ bin/bin.mjs
@@ -5,15 +5,17 @@ const routes = new Map(
     ["setup", "../lib/node/setup.mjs"],
     ["run", "../lib/node/batch.mjs"],
     ["version", "../lib/node/version.mjs"],
+    ["--version", "../lib/node/version.mjs"],
     ["help", "../lib/node/help.mjs"],
+    ["--help", "../lib/node/help.mjs"],
   ],
 );
 
-let route = "run";
+let verb = "run";
 
 if (routes.has(process.argv[2])) {
-  route = process.argv[2];
+  verb = process.argv[2];
   process.argv = [process.argv[0], process.argv[1], ...process.argv.slice(3)];
 }
 
-await import(routes.get(route));
+await import(routes.get(verb));
