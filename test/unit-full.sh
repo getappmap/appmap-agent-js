144e6dcba8c17afe561266c795c52c64ea0fa11a
Laurent Christophe <lachrist@vub.ac.be>
2021-12-23 18:08:01 +0100

    build: extract commands in package.json into script files

---
+++ test/unit-full.sh
@@ -0,0 +1,2 @@
+npx eslint $TURTLE_MAIN $TURTLE_TEST && npx prettier -w $TURTLE_MAIN $TURTLE_TEST
+source test/unit.sh
---
+++ test/unit.sh
@@ -0,0 +1,1 @@
+npx c8 --include $TURTLE_MAIN --check-coverage --branches 100 --functions 100 -- node --unhandled-rejections=strict $TURTLE_TEST
