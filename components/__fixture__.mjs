b78da342600ad26cf7a61238d6ae9d4f9eae3906
Laurent Christophe <lachrist@vub.ac.be>
2021-12-30 17:55:46 +0100

    test: fixture shift from path to URL

--- components/__fixture__.mjs
+++ components/__fixture__.mjs
@@ -1,18 +1,10 @@
 import { strict as Assert } from "assert";
 import { tmpdir as getTemporaryDirectory, platform as getPlatform } from "os";
-import { sep as separator } from "path";
+import { pathToFileURL } from "url";
 
 Error.stackTraceLimit = Infinity;
 
-export const getFreshTemporaryPath = () => [
-  getTemporaryDirectory(),
-  Math.random().toString(36).substring(2)
-].join(separator);
-
-const root = getPlatform() === "win32" ? "C:" : "";
-export const makeAbsolutePath = (...segments) => segments.length === 0
-  ? `${root}${separator}`
-  : [root, ... segments].join(separator);
+export const getFreshTemporaryURL = () => `${pathToFileURL(getTemporaryDirectory())}/${Math.random().toString(36).substring(2)}`;
 
 export const {
   ok: assert,
