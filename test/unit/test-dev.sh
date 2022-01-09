fc75f67efccecce2856ea09315527213a5b94834
Laurent Christophe <lachrist@vub.ac.be>
2022-01-09 15:56:30 +0100

    test: fix broken path

--- test/unit/test-dev.sh
+++ test/unit/test-dev.sh
@@ -1,1 +1,1 @@
-npx eslint $TURTLE_MAIN $TURTLE_TEST && npx prettier -w $TURTLE_MAIN $TURTLE_TEST && source test/unit/test-coverage.sh
+npx eslint $TURTLE_MAIN $TURTLE_TEST && npx prettier -w $TURTLE_MAIN $TURTLE_TEST && source test/unit/test-cov.sh
