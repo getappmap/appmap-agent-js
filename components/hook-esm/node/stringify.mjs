70986982da7154e36b48430186b7420bc262f530
Laurent Christophe <lachrist@vub.ac.be>
2022-09-07 19:29:38 +0200

    doc: add comment about source file encoding

--- components/hook-esm/node/stringify.mjs
+++ components/hook-esm/node/stringify.mjs
@@ -24,6 +24,19 @@ export default (dependencies) => {
         content = toBuffer(content);
       }
       if (content instanceof _Buffer) {
+        // We assume utf8 encoding as node does:
+        // https://github.com/nodejs/node/blob/c200106305f4367ba9ad8987af5139979c6cc40c/lib/internal/modules/cjs/loader.js#L1136
+        //
+        // Module._extensions['.js'] = function(module, filename) {
+        //   // If already analyzed the source, then it will be cached.
+        //   const cached = cjsParseCache.get(module);
+        //   let content;
+        //   if (cached?.source) {
+        //     content = cached.source;
+        //     cached.source = undefined;
+        //   } else {
+        //     content = fs.readFileSync(filename, 'utf8');
+        //   }
         return content.toString("utf8");
       }
       expect(
