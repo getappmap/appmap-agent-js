d4bb18a9b241fbd469fadf8b5ddd3881fb8dbf86
Laurent Christophe <lachrist@vub.ac.be>
2022-09-27 19:27:46 +0200

    refactor: remove useless code to generate schema
    Previously we used to generate a standalone schema file. But this was 
    dropped in favor of runtime dependency.

--- build/schema/index.mjs
+++ build/schema/index.mjs
@@ -5,14 +5,11 @@ import {
 import { fileURLToPath } from "url";
 import { dirname as getDirectory } from "path";
 import YAML from "yaml";
-import Ajv from "ajv";
-import GenerateStandaloneCode from "ajv/dist/standalone/index.js";
 
 const {
   JSON: { stringify: stringifyJSON },
 } = globalThis;
 
-const { default: generateStandaloneCode } = GenerateStandaloneCode;
 const { parse: parseYAML } = YAML;
 
 const __filename = fileURLToPath(import.meta.url);
@@ -21,11 +18,6 @@ const __dirname = getDirectory(__filename);
 const schema = parseYAML(
   await readFileAsync(`${__dirname}/schema.yml`, "utf8"),
 );
-
-const ajv = new Ajv({ code: { source: true }, verbose: true });
-// {code:{source:true}, verbose:true}
-ajv.addSchema(schema);
-generateStandaloneCode(ajv);
 
 await writeFileAsync(
   `${__dirname}/../../dist/schema.mjs`,
