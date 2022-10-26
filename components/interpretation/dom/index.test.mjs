89b28f6be542f78930fe7958529da4dd7eb65cf9
Laurent Christophe <lachrist@vub.ac.be>
2022-10-26 11:24:43 +0200

    test: mock document to test runScript
    Not as good as using JsDom but better than disabling the test.

--- components/interpretation/dom/index.test.mjs
+++ components/interpretation/dom/index.test.mjs
@@ -1,21 +1,43 @@
-/* eslint-env node */
-import { assertEqual } from "../../__fixture__.mjs";
-/* eslint-disable import/no-unresolved */
-import JsDom from "jsdom";
-/* eslint-enable import/no-unresolved */
+import { assertDeepEqual } from "../../__fixture__.mjs";
 
-const { undefined } = globalThis;
-
-const { JSDOM } = JsDom;
-
-const { window } = new JSDOM("", {
-  runScripts: "dangerously",
-});
-
-globalThis.document = window.document;
+globalThis.document = {
+  createElement: (tag) => ({
+    tag,
+    type: null,
+    text: null,
+  }),
+  body: {
+    appendChild: (element) => {
+      assertDeepEqual(element, {
+        tag: "script",
+        type: "text/javascript",
+        text: "123;",
+      });
+    },
+  },
+};
 
 const { runScript } = await import("./index.mjs?env=test");
 
-assertEqual(runScript("var x = 123;", "protocol://host/script.js"), undefined);
+runScript("123;", "protocol://host/path");
 
-assertEqual(window.x, 123);
+// We disable test based on JsDom because it is a large dependency.
+//
+// import { assertEqual } from "../../__fixture__.mjs";
+// import JsDom from "jsdom";
+//
+// const { undefined } = globalThis;
+//
+// const { JSDOM } = JsDom;
+//
+// const { window } = new JSDOM("", {
+//   runScripts: "dangerously",
+// });
+//
+// globalThis.document = window.document;
+//
+// const { runScript } = await import("./index.mjs?env=test");
+//
+// assertEqual(runScript("var x = 123;", "protocol://host/script.js"), undefined);
+//
+// assertEqual(window.x, 123);
