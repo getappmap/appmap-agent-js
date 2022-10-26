feee432e25d74d7f0e35eaf29c001220db15715b
Laurent Christophe <lachrist@vub.ac.be>
2022-10-26 11:07:32 +0200

    test: add colors and use urls in unit test runner

--- test/unit/run.mjs
+++ test/unit/run.mjs
@@ -1,32 +1,53 @@
 import { lstat as lstatAsync, readdir as readdirAsync } from "fs/promises";
-import { join as joinPath } from "path";
 import { spawnAsync } from "../spawn.mjs";
+import Chalk from "chalk";
+import { fileURLToPath } from "url";
 
 const {
+  URL,
   process: { stdout },
   String,
 } = globalThis;
 
-const loop = async (path) => {
-  if (path.endsWith(".test.mjs")) {
-    stdout.write(`${path}...${"\n"}`);
-    const { signal, status } = await spawnAsync("node", [
-      "--unhandled-rejections=strict",
-      path,
-    ]);
+const { green: chalkGreen, blue: chalkBlue, red: chalkRed } = Chalk;
+
+const failures = [];
+
+const loop = async (url) => {
+  if (url.href.endsWith(".test.mjs")) {
+    stdout.write(chalkBlue(`${url.href}...\n`));
+    const { signal, status } = await spawnAsync(
+      "node",
+      ["--unhandled-rejections=strict", fileURLToPath(url)],
+      { stdio: "inherit" },
+    );
     if (signal !== null) {
-      stdout.write(`Killed by ${signal}${"\n"}`);
+      stdout.write(chalkRed(`Killed by ${signal}\n`));
+      failures.push(url.href);
     } else if (status !== 0) {
-      stdout.write(`Exit code ${String(status)}${"\n"}`);
+      stdout.write(chalkRed(`Exit code ${String(status)}\n`));
+      failures.push(url.href);
     } else {
-      stdout.write("Success\n");
+      stdout.write(chalkGreen("Success\n"));
     }
-  } else if ((await lstatAsync(path)).isDirectory()) {
-    for (const filename of await readdirAsync(path)) {
-      await loop(joinPath(path, filename));
+  } else if (url.href.endsWith("/")) {
+    for (const filename of await readdirAsync(url)) {
+      await loop(
+        new URL(
+          (await lstatAsync(new URL(filename, url))).isDirectory()
+            ? `${filename}/`
+            : filename,
+          url,
+        ),
+      );
     }
   }
 };
 
-await loop("./build");
-await loop("./components");
+await loop(new URL("../../components/", import.meta.url));
+
+if (failures.length === 0) {
+  stdout.write(chalkGreen("All passed\n"));
+} else {
+  stdout.write(chalkRed(`Failures:\n  - ${failures.join("\n  - ")}\n`));
+}
