77f613f8114c1ea9a6f017db9979dddc14a93629
Laurent Christophe <lachrist@vub.ac.be>
2022-10-26 10:42:21 +0200

    refactor: remove deadcode for running unit tests

--- test/unit/test-dev.sh
+++
@@ -1,1 +0,0 @@
-npx eslint $TURTLE_MAIN $TURTLE_TEST && npx prettier -w $TURTLE_MAIN $TURTLE_TEST && . test/unit/test-cov.sh
--- test/unit/test.mjs
+++
@@ -1,7 +0,0 @@
-import { sep as path_separator } from "path";
-
-const {
-  process: { env },
-} = globalThis;
-
-await import(`../../${env.TURTLE_TEST.split(path_separator).join("/")}`);
