e122399031d8f1a3314c4c8ba042283931769b2d
Laurent Christophe <lachrist@vub.ac.be>
2022-10-28 16:31:31 +0200

    refactor: update error messages
    [skip ci]

--- components/error/default/index.mjs
+++ components/error/default/index.mjs
@@ -3,16 +3,14 @@ const { URL, Error } = globalThis;
 const { search: __search } = new URL(import.meta.url);
 
 const issues = "https://github.com/getappmap/appmap-agent-js/issues";
-const slack = "https://appmap-group.slack.com/archives/C03GEJVDC2J";
-const discord =
-  "https://discord.com/channels/766016904056930325/818522100205223996";
+const slack = "https://appmap.io/slack";
 const documentation =
   "https://github.com/getappmap/appmap-agent-js/blob/main/REFERENCE.md";
 const instruction = "https://appmap.io/docs/appmap-overview.html";
 
 const internal_message = `
 Detected an internal appmap error.\
- This is probably an issue within the appmap framework.\
+ This is probably an issue with how the AppMap agent is being used.\
  Please consider submitting a bug report at:
   ${issues}
 
@@ -20,15 +18,13 @@ Detected an internal appmap error.\
 
 const external_message = `
 Detected an external appmap error.\
- This is probably a issue with how you used the appmap framework.
+ This is probably an issue with how the AppMap agent is being used.
 - You can look for answers in our installation instructions:
     ${instruction}
 - We also have a reference documentation:
     ${documentation}
 - You can ask for help in our public slack channel:
     ${slack}
-- We also have a public discord:
-    ${discord}
 
 `;
 
