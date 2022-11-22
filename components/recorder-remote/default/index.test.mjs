0a57c7501d3eee48c4f9a5b37c840ccae856a31e
Laurent Christophe <lachrist@vub.ac.be>
2022-11-22 13:14:23 +0100

    refactor: move recorder-cli to consumers
    The remaining logic in `recorder-cli` does not justify the added 
    complexity introduced by this layer. Let's move it to api consumers: 
    `recorder-process`, `recorder-remote`, and `recorder-mocha`.

--- components/recorder-remote/default/index.test.mjs
+++ components/recorder-remote/default/index.test.mjs
@@ -1,4 +1,3 @@
-import { EventEmitter } from "events";
 import "../../__fixture__.mjs";
 import { convertFileUrlToPath } from "../../path/index.mjs?env=test";
 import {
@@ -9,31 +8,25 @@ import { main } from "./index.mjs?env=test";
 
 const {
   process: { version },
-  Error,
-  Object: { assign },
 } = globalThis;
-
-const mock = {
-  cwd: () => convertFileUrlToPath("file:///w:/cwd"),
-  argv: ["node", "main.mjs"],
-  version,
-};
 
 const configuration = createConfiguration("file:///w:/home/");
 
-{
-  const emitter = assign(new EventEmitter(), mock);
+for (const enabled of [true, false]) {
   main(
-    emitter,
+    {
+      cwd: () => convertFileUrlToPath("file:///w:/cwd"),
+      argv: ["node", "main.mjs"],
+      version,
+    },
     extendConfiguration(
       configuration,
       {
+        processes: [{ regexp: "", enabled }],
         recorder: "remote",
         hooks: { cjs: false, esm: false, apply: false, http: false },
       },
       "file:///w:/base/",
     ),
   );
-  emitter.emit("uncaughtExceptionMonitor", new Error("BOUM"));
-  emitter.emit("exit", 123, "SIGINT");
 }
