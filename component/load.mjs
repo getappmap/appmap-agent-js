ac30d883a5bbf534303687b493a49842bb3d73c7
Laurent Christophe <lachrist@vub.ac.be>
2022-11-28 18:19:46 +0100

    fix: use stat instead of import to bypass jest caching
    Jest intercepts module loading and cache the outcome even if the module
    is missing. So when a bundle is missing in the cache, updating the cach
    will not be noticed by jest. To bypass this, we use
    `node:fs/promises.stat` instead of `import`.

--- component/load.mjs
+++ component/load.mjs
@@ -1,21 +1,33 @@
+import { stat as statAsync } from "fs/promises";
 import { hasOwn } from "./util.mjs";
 import { getBundleUrl } from "./layout.mjs";
 import { writeParamsAsync } from "./params.mjs";
 import { routeAsync } from "./route.mjs";
 
-export const loadAsync = async (home, component, params) => {
-  const url = getBundleUrl(home, component, params);
+// It would be better to directory try `await import(bundle_url)`.
+// But jest intercepts import and will remember it is missing on
+// subsequent calls. So updating caching would not be noticed.
+const isMissingAsync = async (url) => {
   try {
-    return await import(url);
+    await statAsync(url);
+    return false;
   } catch (error) {
-    if (!hasOwn(error, "code") || error.code !== "ERR_MODULE_NOT_FOUND") {
+    if (hasOwn(error, "code") && error.code === "ENOENT") {
+      return true;
+    } else {
       throw error;
     }
   }
-  await writeParamsAsync(home, params);
-  await routeAsync(home, params);
-  // Dynamic import to make fast track faster
-  const { bundleAsync } = await import("./bundle.mjs");
-  await bundleAsync(home, component, params);
+};
+
+export const loadAsync = async (home, component, params) => {
+  const url = getBundleUrl(home, component, params);
+  if (await isMissingAsync(url)) {
+    await writeParamsAsync(home, params);
+    await routeAsync(home, params);
+    // Dynamic import to make fast track faster
+    const { bundleAsync } = await import("./bundle.mjs");
+    await bundleAsync(home, component, params);
+  }
   return await import(url);
 };
