f3c3d9416601b897b18baebef53d474abd2716cf
Laurent Christophe <lachrist@vub.ac.be>
2022-12-07 16:08:38 +0100

    refactor: add container component for recorders
    To avoid create a bundle per recorder (they will be 99% identical), we 
    create a single container component containing all the recorders.

---
+++ components/recorder-cli/default/index.mjs
@@ -0,0 +1,3 @@
+export * as RecorderMocha from "../../recorder-mocha/index.mjs";
+export * as RecorderProcess from "../../recorder-process/index.mjs";
+export * as RecorderRemote from "../../recorder-remote/index.mjs";
