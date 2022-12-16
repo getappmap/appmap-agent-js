4920efcd56f0fd6b6d177853e34b29ba588db70d
Laurent Christophe <lachrist@vub.ac.be>
2022-12-16 22:00:09 +0100

    fix: resilient zipping of arguments and parameters
    It was assumed that the number of recorded arguments is always the same 
    as the number of parameters. This is actually not the case when, in the 
    presence of source mapping, the original function does not have the same 
    number of parameters of the generated function. For instance, this can 
    be the case when the agent could not find a function at the indicated 
    position and pick the next best guest. To solve this issue we simply 
    truncate the zip to the smallest array.

--- components/util/default/array.test.mjs
+++ components/util/default/array.test.mjs
@@ -1,8 +1,12 @@
 import { assertDeepEqual } from "../../__fixture__.mjs";
 import { zip } from "./array.mjs";
 
-assertDeepEqual(zip(["foo1", "bar1", "qux1"], ["foo2", "bar2"], "QUX2"), [
+assertDeepEqual(zip(["foo1", "bar1", "qux1"], ["foo2", "bar2"]), [
   ["foo1", "foo2"],
   ["bar1", "bar2"],
-  ["qux1", "QUX2"],
+]);
+
+assertDeepEqual(zip(["foo1", "bar1"], ["foo2", "bar2", "qux2"]), [
+  ["foo1", "foo2"],
+  ["bar1", "bar2"],
 ]);
