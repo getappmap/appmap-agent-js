9a2cffbccd2c1496360f317c7c827977e0c0d6b9
Laurent Christophe <lachrist@vub.ac.be>
2023-01-18 14:27:38 +0100

    test: add env variable to log during testing
    While debugging tests, it is sometimes helpfull access logs. We can now 
    do this by setting the environment variable `APPMAP_TEST_LOGGING`.

--- components/log/test/index.mjs
+++ components/log/test/index.mjs
@@ -1,11 +1,20 @@
-import { format } from "../../util/index.mjs";
+import { stdout, env } from "node:process";
+import { hasOwnProperty, format } from "../../util/index.mjs";
+
+const enabled = hasOwnProperty(env, "APPMAP_TEST_LOGGING");
 
 const log = (template, ...rest) => {
-  format(template, rest);
+  const message = format(template, rest);
+  if (enabled) {
+    stdout.write(`${message}\n`);
+  }
 };
 
 const logWhen = (guard, template, ...rest) => {
-  format(template, rest);
+  const message = format(template, rest);
+  if (guard && enabled) {
+    stdout.write(`${message}\n`);
+  }
   return guard;
 };
 
