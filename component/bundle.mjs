9bef147bdfc64314eb2648cbb9d99b6481e39dc3
Laurent Christophe <lachrist@vub.ac.be>
2023-01-25 10:26:00 +0100

    build: make bundle system compatible with npm git install
    During installation from a git repository, `npm` use a temporary 
    directory such as `appmap-agent-js-fdk3XD7`. So the bundle did not 
    correctly identified external dependencies. We now use `import.meta.url` 
    which is compatible with renaming the root directory.

--- component/bundle.mjs
+++ component/bundle.mjs
@@ -3,7 +3,14 @@ import { rollup } from "rollup";
 import { getComponentMainUrl, getBundleUrl } from "./layout.mjs";
 import { routeAsync } from "./route.mjs";
 
-const { URL } = globalThis;
+const {
+  URL,
+  JSON: { stringify: stringifyJSON },
+} = globalThis;
+
+const { href: internal_prefix } = new URL("../components/", import.meta.url);
+
+const { href: external_prefix } = new URL("../lib/", import.meta.url);
 
 const resolveSpecifier = (specifier, parent, resolved) => {
   if (resolved) {
@@ -21,9 +28,19 @@ const resolveSpecifier = (specifier, parent, resolved) => {
 
 const isExternal = (specifier, parent, resolved) => {
   const maybe_url = resolveSpecifier(specifier, parent, resolved);
-  return (
-    maybe_url === null || !maybe_url.includes("/appmap-agent-js/components/")
-  );
+  if (maybe_url === null) {
+    return true;
+  } else if (maybe_url.startsWith(internal_prefix)) {
+    return false;
+  } else if (maybe_url.startsWith(external_prefix)) {
+    return true;
+  } else {
+    throw new Error(
+      `Unexpected resolved url ${stringifyJSON(maybe_url)} from ${stringifyJSON(
+        specifier,
+      )}`,
+    );
+  }
 };
 
 export const bundleAsync = async (home, component, env, resolution) => {
