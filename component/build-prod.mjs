13cfc30206c08876d0df8ddb866c8fad266ad9a8
Laurent Christophe <lachrist@vub.ac.be>
2023-03-14 11:52:18 +0100

    perf: frontend no longer validate data
    Important validation (mostly user-provided configuration) happens on the backend. Note that `recorder-cli` can still use the `validate` component but it is a no-op. That is because we still want to perform validation in testing.
    
    This is the first part of an effort to reduce exteral dependencies to enable browser support.

--- component/build-prod.mjs
+++ component/build-prod.mjs
@@ -1,18 +1,20 @@
+import { stdout } from "node:process";
 import { home } from "./home.mjs";
 import { bundleAsync } from "./bundle.mjs";
 
 for (const [name, component, env, resolution] of [
   [null, "error", "node", {}],
-  [null, "server", "node", {}],
-  [null, "client", "node", {}],
+  [null, "server", "node", { validate: "ajv" }],
+  [null, "client", "node", { validate: "stub" }],
   [null, "init", "node", {}],
   [null, "status", "node", {}],
-  [null, "setup", "node", {}],
-  [null, "configuration-process", "node", {}],
-  [null, "configuration-environment", "node", {}],
-  [null, "recorder-api", "node", { emitter: "local" }],
-  [null, "recorder-cli", "node", { emitter: "remote-socket" }],
-  [null, "transformer-jest", "node", { emitter: "remote-socket" }],
+  [null, "setup", "node", { validate: "ajv" }],
+  [null, "configuration-process", "node", { validate: "ajv" }],
+  [null, "configuration-environment", "node", { validate: "stub" }],
+  [null, "recorder-api", "node", { emitter: "local", validate: "ajv" }],
+  [null, "recorder-cli", "node", { emitter: "remote-socket", validate: "stub" }],
+  [null, "transformer-jest", "node", { emitter: "remote-socket", validate: "stub" }],
 ]) {
+  stdout.write(`bundling ${component}...\n`);
   await bundleAsync(home, name ?? component, component, env, resolution);
 }
