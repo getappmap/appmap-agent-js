1c5c5b4fb05c6b7fd385e723564603998b216371
Laurent Christophe <lachrist@vub.ac.be>
2023-03-14 11:10:09 +0100

    build: bundles can have a different name than their component

--- component/build-prod.mjs
+++ component/build-prod.mjs
@@ -1,19 +1,19 @@
 import { home } from "./home.mjs";
 import { bundleAsync } from "./bundle.mjs";
 
-for (const [entry, env, resolution] of [
-  ["error", "node", {}],
-  ["server", "node", {}],
-  ["client", "node", {}],
-  ["init", "node", {}],
-  ["status", "node", {}],
-  ["setup", "node", {}],
-  ["configuration", "node", {}],
-  ["configuration-process", "node", {}],
-  ["configuration-environment", "node", {}],
-  ["recorder-api", "node", { emitter: "local" }],
-  ["recorder-cli", "node", { emitter: "remote-socket" }],
-  ["transformer-jest", "node", { emitter: "remote-socket" }],
+for (const [name, component, env, resolution] of [
+  [null, "error", "node", {}],
+  [null, "server", "node", {}],
+  [null, "client", "node", {}],
+  [null, "init", "node", {}],
+  [null, "status", "node", {}],
+  [null, "setup", "node", {}],
+  [null, "configuration", "node", {}],
+  [null, "configuration-process", "node", {}],
+  [null, "configuration-environment", "node", {}],
+  [null, "recorder-api", "node", { emitter: "local" }],
+  [null, "recorder-cli", "node", { emitter: "remote-socket" }],
+  [null, "transformer-jest", "node", { emitter: "remote-socket" }],
 ]) {
-  await bundleAsync(home, entry, env, resolution);
+  await bundleAsync(home, name ?? component, component, env, resolution);
 }
