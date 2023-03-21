f8a35a37ad882796c2d323142c0b17efd5c6af16
Laurent Christophe <lachrist@vub.ac.be>
2023-03-21 11:41:37 +0100

    refactor: implement buffering in websocket and test with real io
    Socket instances should be able to record message straight away.

--- components/socket/browser/index.mjs
+++ components/socket/browser/index.mjs
@@ -1,21 +1,35 @@
 import { InternalAppmapError } from "../../error/index.mjs";
+import { logWarningWhen } from "../../log/index.mjs";
 import { assert } from "../../util/index.mjs";
 
 const { String, WebSocket } = globalThis;
 
-export const openSocket = (host, port) => {
+export const openSocket = (host, port, { "http-switch": segment }) => {
   assert(
     typeof port === "number",
     "cannot use IPC communicate on browser",
     InternalAppmapError,
   );
-  return new WebSocket(`wss://${host}:${String(port)}`);
+  const buffer = [];
+  const socket = new WebSocket(`ws://${host}:${port}/${String(segment)}`);
+  socket.addEventListener("open", () => {
+    for (const message of buffer) {
+      socket.send(message);
+    }
+  });
+  return { socket, buffer };
 };
 
-export const sendSocket = (socket, message) => {
-  socket.send(message);
+export const sendSocket = ({ socket, buffer }, message) => {
+  if (socket.readyState === 0) {
+    buffer.push(message);
+  } else if (socket.readyState === 1) {
+    socket.send(message);
+  } else {
+    logWarningWhen("Lost message >> %j", message);
+  }
 };
 
-export const closeSocket = (socket) => {
+export const closeSocket = ({ socket }) => {
   socket.close();
 };
--- components/socket/browser/index.test.mjs
+++ components/socket/browser/index.test.mjs
@@ -1,29 +1,71 @@
 /* eslint-env node */
+import { default as WebSocket, WebSocketServer } from "ws";
+import { assertEqual } from "../../__fixture__.mjs";
+import {
+  createConfiguration,
+  extendConfiguration,
+} from "../../configuration/index.mjs";
 import { defineGlobal } from "../../global/index.mjs";
-import { assertDeepEqual } from "../../__fixture__.mjs";
 
-const trace = [];
+const { Promise } = globalThis;
 
-defineGlobal("WebSocket", function (url) {
-  trace.push(["open", url]);
-  return {
-    send(message) {
-      trace.push(["send", message]);
-    },
-    close() {
-      trace.push(["close"]);
-    },
-  };
-});
+defineGlobal("WebSocket", WebSocket);
 
 const { openSocket, sendSocket, closeSocket } = await import("./index.mjs");
 
-const socket = openSocket("host", 8080);
-sendSocket(socket, "message");
+const server = new WebSocketServer({ port: 0 });
+
+await new Promise((resolve, reject) => {
+  server.on("listening", resolve);
+  server.on("error", reject);
+});
+
+const socket = openSocket(
+  "localhost",
+  server.address().port,
+  extendConfiguration(
+    createConfiguration("protocol://host/home"),
+    {
+      "http-switch": "__appmap__",
+    },
+    null,
+  ),
+);
+
+sendSocket(socket, "connecting");
+
+const ws = await new Promise((resolve, reject) => {
+  server.on("error", reject);
+  server.on("connection", (ws, req) => {
+    assertEqual(req.url, "/__appmap__");
+    resolve(ws);
+  });
+});
+
+assertEqual(
+  (
+    await new Promise((resolve, reject) => {
+      ws.on("error", reject);
+      ws.on("message", resolve);
+    })
+  ).toString("utf8"),
+  "connecting",
+);
+
+sendSocket(socket, "open");
+
+assertEqual(
+  (
+    await new Promise((resolve, reject) => {
+      ws.on("error", reject);
+      ws.on("message", resolve);
+    })
+  ).toString("utf8"),
+  "open",
+);
+
 closeSocket(socket);
 
-assertDeepEqual(trace, [
-  ["open", "wss://host:8080"],
-  ["send", "message"],
-  ["close"],
-]);
+sendSocket(socket, "closing");
+
+server.close();
