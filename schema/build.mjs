c0ac6a7d5724bc46770d744078d1ceb7b4ecbf80
Laurent Christophe <lachrist@vub.ac.be>
2023-03-21 10:26:42 +0100

    refactor: improve scema/build.mjs modularity

--- schema/build.mjs
+++ schema/build.mjs
@@ -15,40 +15,45 @@ const {
 
 const { url: __url } = import.meta;
 
-const schemas = [];
+const loadSchemaArrayAsync = async (directory) => {
+  const schemas = [];
+  for (const filename of await readdirAsync(new URL(directory))) {
+    schemas.push({
+      $id: filename.split(".")[0],
+      ...parseYAML(await readFileAsync(new URL(filename, directory), "utf8")),
+    });
+  }
+  return schemas;
+};
 
-for (const filename of await readdirAsync(new URL("definitions", __url))) {
-  schemas.push({
-    $id: filename.split(".")[0],
-    ...parseYAML(
-      await readFileAsync(new URL(`definitions/${filename}`, __url), "utf8"),
-    ),
+const compileSchemaArray = (schemas, root) => {
+  const ajv = new Ajv({
+    schemas,
+    verbose: true,
+    code: { source: true, esm: true },
   });
-}
+  return generateStandaloneModule(ajv, root);
+};
 
-await mkdirAsync(new URL("../dist", __url), { recursive: true });
-
-const ajv = new Ajv({
-  schemas,
-  verbose: true,
-  code: { source: true, esm: true },
-});
-
-await writeFileAsync(
-  new URL("../dist/schema.mjs", __url),
-  generateStandaloneModule(ajv, {
+{
+  const schemas = await loadSchemaArrayAsync(new URL("definitions/", __url));
+  await mkdirAsync(new URL("../dist", __url), { recursive: true });
+  const root = {
     validateSerial: "serial",
     validatePayload: "payload",
     validateExternalConfiguration: "configuration-external",
     validateInternalConfiguration: "configuration-internal",
     validateMessage: "message",
     validateSourceMap: "source-map",
-  }),
-  "utf8",
-);
-
-await writeFileAsync(
-  new URL("../dist/schema.json", __url),
-  stringifyJSON(schemas, null, 2),
-  "utf8",
-);
+  };
+  await writeFileAsync(
+    new URL("../dist/schema.mjs", __url),
+    compileSchemaArray(schemas, root),
+    "utf8",
+  );
+  await writeFileAsync(
+    new URL("../dist/schema.json", __url),
+    stringifyJSON(schemas, null, 2),
+    "utf8",
+  );
+}
