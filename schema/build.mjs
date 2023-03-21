4f049291d6977f8af1c93d96f5251899a948816d
Laurent Christophe <lachrist@vub.ac.be>
2023-03-21 10:37:54 +0100

    refactor: detect and remove unused schemas

--- schema/build.mjs
+++ schema/build.mjs
@@ -4,12 +4,16 @@ import {
   readFile as readFileAsync,
   writeFile as writeFileAsync,
 } from "node:fs/promises";
+import { stdout } from "node:process";
 import { URL } from "node:url";
 import { default as Ajv } from "ajv";
 import { default as generateStandaloneModule } from "ajv/dist/standalone/index.js";
 import { parse as parseYAML } from "yaml";
 
 const {
+  Set,
+  Reflect: { ownKeys },
+  Array: { isArray },
   JSON: { stringify: stringifyJSON },
 } = globalThis;
 
@@ -35,6 +39,39 @@ const compileSchemaArray = (schemas, root) => {
   return generateStandaloneModule(ajv, root);
 };
 
+const findDeadSchemaArray = (schemas, ids) => {
+  const used = new Set();
+  const useId = (id) => {
+    if (!used.has(id)) {
+      used.add(id);
+      for (const schema of schemas) {
+        if (schema.$id === id) {
+          /* eslint-disable no-use-before-define */
+          use(schema);
+          /* eslint-enable no-use-before-define */
+        }
+      }
+    }
+  };
+  const use = (json) => {
+    if (isArray(json)) {
+      json.forEach(use);
+    } else if (typeof json === "object" && json !== null) {
+      for (const key of ownKeys(json)) {
+        if (key === "$ref") {
+          useId(json[key]);
+        } else {
+          use(json[key]);
+        }
+      }
+    }
+  };
+  for (const id of ids) {
+    useId(id);
+  }
+  return schemas.map((schema) => schema.$id).filter((id) => !used.has(id));
+};
+
 {
   const schemas = await loadSchemaArrayAsync(new URL("definitions/", __url));
   await mkdirAsync(new URL("../dist", __url), { recursive: true });
@@ -56,4 +93,11 @@ const compileSchemaArray = (schemas, root) => {
     stringifyJSON(schemas, null, 2),
     "utf8",
   );
+  const deads = findDeadSchemaArray(schemas, [
+    "config",
+    ...ownKeys(root).map((key) => root[key]),
+  ]);
+  if (deads.length > 0) {
+    stdout.write(`unused schemas: ${deads.join(", ")}\n`);
+  }
 }
