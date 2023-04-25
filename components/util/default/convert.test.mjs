1416486c2c6855028723ad63624b6ad5dd71ff47
Laurent Christophe <lachrist@vub.ac.be>
2023-04-25 16:09:12 +0200

    fix: use trully pure convertions
    Actually, `Object.prototype.toString` is impure as it invokes 
    `Symbol.toStringTag`. Using a pure convertions makes the agent more 
    transparent and less likely to affect the observed application.

--- components/util/default/convert.test.mjs
+++ components/util/default/convert.test.mjs
@@ -2,12 +2,15 @@ import { assertEqual } from "../../__fixture__.mjs";
 import {
   toBoolean,
   toNumber,
+  toInteger,
   jsonifyNumber,
   print,
   toString,
 } from "./convert.mjs";
 
 const {
+  Number,
+  BigInt,
   Number: { NaN, NEGATIVE_INFINITY, POSITIVE_INFINITY },
 } = globalThis;
 
@@ -15,11 +18,47 @@ const {
 assertEqual(toBoolean(1), true);
 assertEqual(toBoolean(0), false);
 
+// toInteger //
+assertEqual(toInteger(false), 0);
+assertEqual(toInteger(true), 1);
+assertEqual(toInteger(123), 123);
+assertEqual(toInteger(Number(`1${"0".repeat(100)}`)), POSITIVE_INFINITY);
+assertEqual(toInteger(Number(`-1${"0".repeat(100)}`)), NEGATIVE_INFINITY);
+assertEqual(toInteger(123.4), 123);
+assertEqual(toInteger("123"), 123);
+assertEqual(toInteger("123.4"), 123);
+assertEqual(toInteger(123n), 123);
+assertEqual(toInteger(BigInt(`1${"0".repeat(100)}`)), POSITIVE_INFINITY);
+assertEqual(toInteger(BigInt(`-1${"0".repeat(100)}`)), NEGATIVE_INFINITY);
+assertEqual(toInteger(null), NaN);
+
 // toNumber //
+assertEqual(toNumber(false), 0);
+assertEqual(toNumber(true), 1);
 assertEqual(toNumber(123), 123);
 assertEqual(toNumber("123"), 123);
 assertEqual(toNumber(123n), 123);
 assertEqual(toNumber(null), NaN);
+
+// toString //
+assertEqual(
+  toString(function f() {}),
+  "[function]",
+);
+assertEqual(toString([]), "[array]");
+assertEqual(toString({}), "[object]");
+assertEqual(toString(123), "123");
+assertEqual(toString("foo"), "foo");
+
+// toString //
+assertEqual(
+  print(function f() {}),
+  "[function]",
+);
+assertEqual(print([]), "[array]");
+assertEqual(print({}), "[object]");
+assertEqual(print(123), "123");
+assertEqual(print("foo"), '"foo"');
 
 // jsonifyNumber //
 {
@@ -33,12 +72,3 @@ assertEqual(toNumber(null), NaN);
   assertEqual(jsonifyNumber(NEGATIVE_INFINITY, replacements), -1);
   assertEqual(jsonifyNumber(POSITIVE_INFINITY, replacements), 1);
 }
-
-// print //
-assertEqual(print(true), "true");
-assertEqual(print("foo"), '"foo"');
-assertEqual(print([]), "[object Array]");
-
-// toString //
-assertEqual(toString(true), "true");
-assertEqual(toString("foo"), "foo");
