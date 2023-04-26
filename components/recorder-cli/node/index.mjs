773839735e0a04d69e4f184602b6f61224e911a9
Laurent Christophe <lachrist@vub.ac.be>
2023-04-26 17:28:30 +0200

    refactor: merge remote and process recorders for node
    These two recorders are very similar and can be merged to reduce code 
    duplication.

--- components/recorder-cli/node/index.mjs
+++ components/recorder-cli/node/index.mjs
@@ -1,15 +1,18 @@
 import process from "node:process";
 import { createServer } from "node:http";
 import { hook } from "../../hook/index.mjs";
+import { InternalAppmapError } from "../../error/index.mjs";
 import { getUuid } from "../../uuid/index.mjs";
 import { logInfo } from "../../log/index.mjs";
 import { generateRespond, requestAsync } from "../../http/index.mjs";
+import { extendConfiguration } from "../../configuration/index.mjs";
 import {
   extendConfigurationNode,
   isConfigurationEnabled,
 } from "../../configuration-accessor/index.mjs";
 import {
   createFrontend,
+  recordStartTrack,
   flush as flushFrontend,
 } from "../../frontend/index.mjs";
 import {
@@ -27,18 +30,12 @@ const {
 export const record = (configuration) => {
   configuration = extendConfigurationNode(configuration, process);
   if (isConfigurationEnabled(configuration)) {
-    logInfo(
-      "Enabling remote recording on process #%j -- %j",
-      process.pid,
-      process.argv,
-    );
-    logInfo(
-      "Remote recording api documention: https://appmap.io/docs/reference/remote-recording-api.html",
-    );
     if (configuration.session === null) {
       configuration = { ...configuration, session: getUuid() };
     }
     const {
+      session,
+      recorder,
       heartbeat,
       host,
       "frontend-track-port": frontend_track_port,
@@ -61,24 +58,47 @@ export const record = (configuration) => {
     process.on("exit", flush);
     process.on("uncaughtExceptionMonitor", flush);
     openSocketAsync(socket).then(flush);
-    if (frontend_track_port !== null) {
-      const server = createServer();
-      server.unref();
-      /* c8 ignore start */
-      server.on(
-        "request",
-        generateRespond((method, path, body) =>
-          requestAsync(
-            host,
-            backend_track_port,
-            method,
-            `/_appmap${path}`,
-            body,
-          ),
-        ),
+    if (recorder === "remote") {
+      logInfo(
+        "Enabling remote recording on process #%j -- %j",
+        process.pid,
+        process.argv,
       );
-      /* c8 ignore start */
-      server.listen(frontend_track_port);
+      logInfo(
+        "Remote recording api documention: https://appmap.io/docs/reference/remote-recording-api.html",
+      );
+      if (frontend_track_port !== null) {
+        const server = createServer();
+        server.unref();
+        /* c8 ignore start */
+        server.on(
+          "request",
+          generateRespond((method, path, body) =>
+            requestAsync(
+              host,
+              backend_track_port,
+              method,
+              `/_appmap${path}`,
+              body,
+            ),
+          ),
+        );
+        /* c8 ignore start */
+        server.listen(frontend_track_port);
+      }
+    } else if (recorder === "process") {
+      logInfo(
+        "Recording the entire process #%j -- %j",
+        process.pid,
+        process.argv,
+      );
+      recordStartTrack(
+        frontend,
+        `process-${getUuid()}`,
+        extendConfiguration(configuration, { sessions: session }, null),
+      );
+    } else {
+      throw new InternalAppmapError("invalid node recorder");
     }
   } /* c8 ignore start */ else {
     logInfo("Not recording process #%j -- %j", process.pid, process.argv);
---
+++ components/recorder-cli/node/index.test.mjs
@@ -0,0 +1,34 @@
+import { cwd } from "node:process";
+import "../../__fixture__.mjs";
+import { toDirectoryUrl } from "../../url/index.mjs";
+import { convertPathToFileUrl } from "../../path/index.mjs";
+import {
+  createConfiguration,
+  extendConfiguration,
+} from "../../configuration/index.mjs";
+import { record } from "./index.mjs";
+
+const base = toDirectoryUrl(convertPathToFileUrl(cwd()));
+
+for (const recorder of ["process", "remote"]) {
+  record(
+    extendConfiguration(
+      createConfiguration("file:///w:/home/"),
+      {
+        processes: [{ regexp: "", enabled: true }],
+        recorder,
+        hooks: {
+          cjs: false,
+          esm: false,
+          eval: false,
+          apply: false,
+          http: false,
+          mysql: false,
+          pg: false,
+          sqlite3: false,
+        },
+      },
+      base,
+    ),
+  );
+}
